cbcedd576d2108a9395dd79896079011da2fce7d
ayadij <ayadijkwel@yahoo.com>
2018-04-03 20:06:44 -0600

    changed data type in schema from 'int' to 'integer' and migrated the database

--- db/migrate/20180404014957_create_vehicles.rb
+++ db/migrate/20180404014957_create_vehicles.rb
@@ -2,18 +2,18 @@ class CreateVehicles < ActiveRecord::Migration[5.1]
   def change
     create_table :vehicles do |t|
       t.string :name
-      t.int :model_year
+      t.integer :model_year
       t.string :make
       t.string :model
       t.string :trim_level
       t.string :body_style
       t.string :engine_type
-      t.int :milage
+      t.integer :milage
       t.string :vin
       t.text :notes
       t.date :bought_date
-      t.int :bought_milage
-      t.int :bought_price
+      t.integer :bought_milage
+      t.integer :bought_price
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180404014957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "name"
+    t.integer "model_year"
+    t.string "make"
+    t.string "model"
+    t.string "trim_level"
+    t.string "body_style"
+    t.string "engine_type"
+    t.integer "milage"
+    t.string "vin"
+    t.text "notes"
+    t.date "bought_date"
+    t.integer "bought_milage"
+    t.integer "bought_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
