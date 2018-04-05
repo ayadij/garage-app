63f379713c09f8a96fdb389597a1251f39a7dab6
ayadij <ayadijkwel@yahoo.com>
2018-04-05 10:43:24 -0600

    created session helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def session_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      link_to "Login", new_user_session_path
+    end 
+  end
+
 end
+ 
