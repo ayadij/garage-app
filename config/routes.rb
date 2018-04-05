532248e3a6675e2952c6dd339cb8c48988a925e7
ayadij <ayadijkwel@yahoo.com>
2018-04-04 22:18:04 -0600

    customized login, logout, and register routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   get 'home', to: 'pages#home'
   get 'garage', to: 'pages#garage'
   get 'vin-decoder', to: 'pages#vin_decoder'
