d6f7889bbf2c3cb292a4109a568056195a9dde44
ayadij <ayadijkwel@yahoo.com>
2018-04-09 10:48:24 -0600

    styles and prep for navbar implementation

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,9 @@
 module PagesHelper
 
   def copyright_generator
-    DijkwelViewTool::Renderer.copyright 'Dijkwel', 'All rights reserved.'
+    if current_user.is_a?(GuestUser)
+      DijkwelViewTool::Renderer.copyright 'Dijkwel', 'All rights reserved.'
+    end 
   end
 
 end
