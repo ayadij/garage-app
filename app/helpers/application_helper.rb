89bbd7752b1ac56e3a03a48183295da759221246
ayadij <ayadijkwel@yahoo.com>
2018-04-09 15:07:09 -0600

    finished nav partial function and style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def session_helper
     if current_user.is_a?(GuestUser)
-      link_to "Login", new_user_session_path
+      link_to "Login", new_user_session_path, class: "nav-link"
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: "nav-link"
     end 
   end
 
