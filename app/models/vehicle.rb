c4f2093eabf19e99cb100462b112b7a7f40ed594
ayadij <ayadijkwel@yahoo.com>
2018-04-19 15:14:17 -0600

    implemented initail file uploading system via carrierwave for vehicles and moved the update form button

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,14 @@
 class Vehicle < ApplicationRecord
 
-  validates_presence_of :name
+  #include Placeholder
+  validates_presence_of :name, :image
 
   mount_uploader :image, VehicleUploader
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.image ||= Placeholder.image_generator(height: '400', width: '600')
+  end
+
 end
