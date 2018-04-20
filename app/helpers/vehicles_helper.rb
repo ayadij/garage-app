c3fde08e838a467ade9c581b891d177be5499d72
ayadij <ayadijkwel@yahoo.com>
2018-04-20 12:25:41 -0600

    edited vehicle form page and helper

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -1,2 +1,18 @@
 module VehiclesHelper
+
+
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def vehicle_img img, type
+    if img
+      img
+    else
+      image_generator(height: '300', width: '400')
+    end
+  end
+
+
+
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,14 +1,8 @@
 class Vehicle < ApplicationRecord
 
-  #include Placeholder
-  validates_presence_of :name, :image
+  validates_presence_of :name
 
   mount_uploader :image, VehicleUploader
 
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.image ||= Placeholder.image_generator(height: '400', width: '600')
-  end
 
 end
